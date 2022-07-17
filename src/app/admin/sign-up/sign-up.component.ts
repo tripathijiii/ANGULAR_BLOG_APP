d6a54203ceb5ddbcc2252c30c185300b7abef992
tripathijiiiii <shashwatesht@gmail.com>
2022-07-17 13:11:13 +0530

    singup complete

--- src/app/admin/sign-up/sign-up.component.ts
+++ src/app/admin/sign-up/sign-up.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {Router} from '@angular/router';
@@ -8,16 +9,23 @@ import {Router} from '@angular/router';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor(private route:Router) { }
+  constructor(private route:Router,private http:HttpClient) { }
 
   ngOnInit(): void {
   }
   onSignUP(form: NgForm){
-    localStorage.setItem('userEmail',form.value.title);
-    localStorage.setItem('password',form.value.content);
-    localStorage.setItem('password',form.value.content);
-    alert("this is bad")
-    return;
-    this.route.navigate(['/user']);
+    this.http.post<{message:String,posts:[]}>("http://localhost:3000/api/useremailcheck",{email:form.value.title}).subscribe((response)=>{
+      if(response.posts.length>0){
+        alert("This email Already exists !!! Sign In !");
+        return;
+      }
+      else{
+        this.http.post<{message:String}>("http://localhost:3000/api/createuser",{email:form.value.title,password:form.value.content,username:form.value.username}).subscribe((response)=>{
+          alert("User Created !!!");
+          this.route.navigate(['']);
+        })
+      }
+    })
   }
 }
+ 
